e2af6c0e05da34283bd1a652c95225183ff69a01
Gabriel Onishi <gabriel.onishi@outlook.com>
2021-10-23 19:01:00 -0300

    Implementei odometria, organizei arquivos

--- projeto-mwrg/scripts/control.py
+++ projeto-mwrg/scripts/control.py
@@ -10,19 +10,35 @@ import sys
 from sensor_msgs.msg import Image, CompressedImage, LaserScan
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
-from cv_bridge import CvBridge, CvBridgeError
+from nav_msgs.msg import Odometry
 
-class Follower:
+# Dados de Odometria
+x = -1
+y = -1
+z = -1
+
+def recebeu_leitura(dado):
+    """
+        Grava nas variáveis x,y,z a posição extraída da odometria
+        Atenção: *não coincidem* com o x,y,z locais do drone
+    """
+    global x
+    global y 
+    global z 
+
+    x = dado.pose.pose.position.x
+    y = dado.pose.pose.position.y
+    z = dado.pose.pose.position.z
+
+class Controller:
 
     def __init__(self):
-        
-        self.bridge = CvBridge()
-        self.cv_image = None
 
         #Publishers e subscribers
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self.laser_subscriber = rospy.Subscriber('/scan', LaserScan, self.laser_callback)                   
         self.dif_subscriber = rospy.Subscriber('/dif', String, self.atualiza_dif)
+
         
         self.twist = Twist()
         self.laser_msg = LaserScan()
@@ -43,24 +59,37 @@ class Follower:
     def get_laser(self, pos):
         return self.laser_msg.ranges[pos]
     
-    def control(self):        
+    def control_go(self):        
         #Controle P simples
         self.twist.linear.x = 0.2
         self.twist.angular.z = - self.dif / 100
-        #END CONTROL
         
         #publica velocidade
         self.cmd_vel_pub.publish(self.twist)
         #rospy.loginfo("linear: %f angular: %f", self.twist.linear.x, self.twist.angular.z)
         self.rate.sleep()
     
+    def control_stop(self):
+        self.twist.linear.x = 0
+        self.cmd_vel_pub.publish(self.twist)
+        self.rate.sleep()
+        rospy.sleep(5)
     
-
 # Main loop
 if __name__== "__main__":
-    rospy.init_node('MandaNodes')
-    follower = Follower()
+    rospy.init_node('Controller')
+    controller = Controller()
+    recebe_scan = rospy.Subscriber('/odom', Odometry , recebeu_leitura)
+    deuVolta = False
 
     while not rospy.is_shutdown():
-        follower.control()
+        print("x: {} y: {};".format(round(x,2), round(y,2)))
+        print(f'Deu volta> {deuVolta}')
+        if x>0.5 and y<1:
+            deuVolta = True
+        if deuVolta == True and x>-0.15 and x<0.15 and y>-0.15 and y<0.15:
+            controller.control_stop()
+        controller.control_go()
+        
+
         
--- projeto-mwrg/scripts/image.py
+++ projeto-mwrg/scripts/image.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+
 import sys
+from numpy.lib.twodim_base import _trilu_dispatcher
 import rospy
 import numpy as np
 import math
@@ -12,7 +14,7 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
-#ARUCO: frente - 200, esquerda - 13, direita - 50
+#print(sys.argv)
 class Image_converter:
 
     def __init__(self):
@@ -21,17 +23,18 @@ class Image_converter:
         self.bridge = CvBridge()
         self.cv_image = None
         self.image_sub = rospy.Subscriber('/camera/image/compressed', CompressedImage, self.image_callback, queue_size=4, buff_size = 2**24)
-        self.publica_dif = rospy.Publisher('/dif', String, queue_size=10)
+        self.publica_dif = rospy.Publisher('/dif', String, queue_size=1)
         self.w = -1
         self.h = -1
         self.cx = -1
         self.cy = -1
         self.dif = -1
-        self.proxesquerda = True
+        self.proxdireita = False
 
     def image_callback(self, msg):
         
         try:
+            #Tratando as imagens
             cv_image = self.bridge.compressed_imgmsg_to_cv2(msg,desired_encoding='bgr8')
             hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
             gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
@@ -52,34 +55,33 @@ class Image_converter:
             mask[0:search_top, 0:w] = 0
             mask[search_bot:h, 0:w] = 0
 
-            # Alterando as imagens para o caso do primeiro aruco (frontal)
             if ids is not None and len(ids)>0:
-                #print(ids)
-                if self.proxesquerda and ids[0] == [200]:
+                aresta = abs(corners[0][0][0][0] - corners[0][0][1][0])
+                if not self.proxdireita and ids[0] == [200] and aresta > 30:
                     # bloqueia a parte direita da imagem (vira a esquerda)
                     mask[search_top:search_bot, 3*w//5:w] = 0
-                if not self.proxesquerda and ids[0] == [200]:
+                elif self.proxdireita and ids[0] == [200] and aresta > 30:
                     # bloqueia a parte esquerda da imagem (vira a direita)
                     mask[search_top:search_bot, 0:3*w//5] = 0
-                if ids[0] == [100]:
-                    self.proxesquerda = False
+                elif ids[0] == [100]:
                     mask[search_top:search_bot, 3*w//5:w] = 0
+                    self.proxdireita = True
 
-            print(self.proxesquerda)
-            cv2.imshow('hi', mask)
-            self.w = w
-            self.h = h
-
+            # Achando centro de massa dos pontos amarelos
             M = cv2.moments(mask)
 
             if M['m00'] > 0:
                 self.cx = int(M['m10']/M['m00'])
                 self.cy = int(M['m01']/M['m00'])
                 cv2.circle(cv_image, (self.cx, self.cy), 10, (0,0,255), -1)  
-                
+            
+            #Atualizando atributos e publicando dif
+            self.w = w
+            self.h = h   
             self.dif = self.cx - self.w/2
-
             self.publica_dif.publish(str(self.dif))
+
+            cv2.imshow('mask', mask)
             cv2.imshow("window", cv_image)
             cv2.waitKey(1)
 
